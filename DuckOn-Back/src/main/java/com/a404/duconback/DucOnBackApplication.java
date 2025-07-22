4f5ff6ec67461fc07f45406ea9d523073265566b
wnsqja7545 <wnsqja7545@gmail.com>
2025-07-22 11:32:41 +0900

    init

---
+++ DuckOn-Back/src/main/java/com/a404/duconback/DucOnBackApplication.java
@@ -0,0 +1,13 @@
+package com.a404.duconback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DucOnBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DucOnBackApplication.class, args);
+    }
+
+}
---
+++ DuckOn-Back/src/test/java/com/a404/duconback/DucOnBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a404.duconback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DucOnBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
